84ee5773149e6daf8a0fb9de0d7ffd22bdc0d926
yashika0810 <khatri.yashika48@gmail.com>
2019-05-03 05:33:09 -0700

    multiple codes

---
+++ input.py
@@ -0,0 +1,37 @@
+'''a = False
+if a:
+    print('weird')
+else:
+    print('not weird')
+
+
+
+
+x= int(input('please enter an integer'))
+if x<0:
+    print('result is negative')
+elif x==0:
+    print('result is o')
+elif x>6:
+    print('result is positive')
+else:
+    print('other')
+    '''
+
+
+
+'''
+mylist =[1,2,3,4,5,44,33]
+for obj in mylist:
+    print('list is',obj)
+'''
+
+
+
+mylist =[1,2,3,4,5,6]
+for obj in mylist:
+    if obj%2 ==0:
+        print(obj)
+    else:
+        print(f'odd number: {obj}')
+
